7db74cfee2830970ec0e623cf392bac53c6e8e6b
adfhsjt <ahmadriski207@gmail.com>
2025-02-17 13:04:48 +0700

    feat(Routing) : Macam-macam Routing pada Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,91 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Bassic Route
+
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+
+Route::get('/about', function () {
+    return 'NIM : 2341720106 <br> Nama : Ahmad Dzul Fadhli Hannan';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+// Route Parameter
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya ' . $name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID {' . $id . '}';
+});
+
+// Optional Parameters
+
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya ' . $name;
+// });
+
+// Route::get('/user/{name?}', function ($name = 'John') {
+//     return 'Nama saya ' . $name;
+// });
+
+// Route Name
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+// Route Group dan Route Prefixes
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+// Route Prefixes
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+// Redirect Routes
+
+Route::redirect('/here', '/there');
+
+// View Routes
+
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+
